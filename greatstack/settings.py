68d272cd4405509d0d3c5d0dfaf138f300290fc9
Siva Kumar <sivakumarjeevavantham@gmail.com>
2025-05-05 11:03:04 +0530

    publicipaddress

--- greatstack/settings.py
+++ greatstack/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)  # True
 
-ALLOWED_HOSTS = ['django-greatkart-env.eba-njd3akq4.us-west-2.elasticbeanstalk.com',
-                 "*", "djangoecommerceapp.online"]
+ALLOWED_HOSTS = ['3.110.158.10', '127.0.0.1']
 #
 # Application definition
 
